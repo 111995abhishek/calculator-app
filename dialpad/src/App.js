34568d50e1a6b0acf12bdf20e9c4e7fb11deb0a4
abhishek <111995abhishek@gmail.com>
2019-11-28 12:25:42 +0530

    added anonyms method

--- dialpad/src/App.js
+++ dialpad/src/App.js
@@ -8,70 +8,68 @@ class Dialpad extends React.Component{
     value : this.props.value
 
   };
-  this.numberone=this.numberone.bind(this);
-  this.numbertwo=this.numbertwo.bind(this);
-  this.numberthree=this.numberthree.bind(this);
-  this.numberfour=this.numberfour.bind(this);
-  this.numberfive=this.numberfive.bind(this);
-  this.numbersix=this.numbersix.bind(this);
-  this.numberseven=this.numberseven.bind(this);
-  this.numbereight=this.numbereight.bind(this);
-  this.numbernine=this.numbernine.bind(this);
-  this.numberzero=this.numberzero.bind(this);
+  // this.numberone=this.numberone.bind(this);
+  // this.numbertwo=this.numbertwo.bind(this);
+  // this.numberthree=this.numberthree.bind(this);
+  // this.numberfour=this.numberfour.bind(this);
+  // this.numberfive=this.numberfive.bind(this);
+  // this.numbersix=this.numbersix.bind(this);
+  // this.numberseven=this.numberseven.bind(this);
+  // this.numbereight=this.numbereight.bind(this);
+  // this.numbernine=this.numbernine.bind(this);
+  // this.numberzero=this.numberzero.bind(this);
   this.TakeEvent=this.TakeEvent.bind(this);
 }
-numberone(){
-  this.setState({
-    value:this.state.value + "1"
+// numberone(){
+  // this.numberone=this.numberone.bind(this);
+  // this.numbertwo=this.numbertwo.bind(this);
+  // this.numberthree=this.numberth
+//   this.setState({
+//     value:this.state.value + "1"
 
-  });
-}
-numbertwo(){
-  this.setState({
-    value:this.state.value + "2"
-  });
-}
-numberthree(){
-  this.setState({
-    value:this.state.value + "3"
-  });
-}
-numberfour(){
-  this.setState({
-    value:this.state.value + "4"
-  });
-}
-numberfive(){
-  this.setState({
-    value:this.state.value + "5"
-  });
-}
-numbersix(){
-  this.setState({
-    value:this.state.value + "6"
-  });
-}
-numberseven(){
-  this.setState({
-    value:this.state.value + "7"
-  });
-}
-numbereight(){
-  this.setState({
-    value:this.state.value + "8"
-  });
-}
-numbernine(){
-  this.setState({
-    value:this.state.value + "9"
-  });
-}
-numberzero(){
-  this.setState({
-    value:this.state.value + "0"
+//   });
+// }
+// numbertwo(){
+//   this.setState({
+//     value:this.state.value + "2"
+//   });
+// }
+// numberthree(){
+//   this.setState({
+//     value:this.state.value + "3"
+  // this.numberone=this.numberone.bind(this);
+  // this.numbertwo=this.numbertwo.bind(this);
+  // this.numberthree=this.numberth
+//   this.setState({
+//     value:this.state.value + "5"
+//   });
+// }
+// numbersix(){
+//   this.setState({
+//     value:this.state.value + "6"
+//   });
+// }
+// numberseven(){
+//   this.setState({
+//     value:this.state.value + "7"
+//   });
+// }
+// numbereight(){
+//   this.setState({
+//     value:this.state.value + "8"
+//   });
+// }
+// numbernine(){
+//   this.setState({
+//     value:this.state.value + "9"
+//   });
+// }
+// numberzero(){
+//   this.setState({
+//     value:this.state.value + "0"
 
-  });
-}
+//   });
+// }
 TakeEvent(event){
   let value=event.target.value;
   if (value){
@@ -87,26 +85,32 @@ TakeEvent(event){
     });
   }
 }
+Plus(){
+  let temp
+  
+
+  
+}
 
   render(){
     return(
       <div class="calling-screen"> 
-      <input type="text" value={this.state.value} onChange={this.TakeEvent}></input>
+      <input type="text" value={this.state.value} onChange={this.TakeEvent}/>
       <div class="dialling-number">
-        <button className="button1" onClick={this.numberseven}>7</button>
-        <button className="button1" onClick={this.numbereight}>8</button>
-        <button className="button1" onClick={this.numbernine}>9</button><br></br>
-        <button className="button1" onClick={this.numberfour}>4</button>
-        <button className="button1" onClick={this.numberfive}>5</button>
-        <button className="button1" onClick={this.numbersix}>6</button><br></br>
-        <button className="button1" onClick={this.numberone}>1</button>
-        <button className="button1" onClick={this.numbertwo}>2</button>
-        <button className="button1" onClick={this.numberthree}>3</button><br></br>
-        <button className="button1" onClick={this.numberzero}>0</button>
-
+        <button className="button1" onClick={()=>this.setState({value:this.state.value+'7'})}>7</button>
+        <button className="button1" onClick={()=>this.setState({value:this.state.value+'8'})}>8</button>
+        <button className="button1" onClick={()=>this.setState({value:this.state.value+'9'})}>9</button><br></br>
+        <button className="button1" onClick={()=>this.setState({value:this.state.value+'4'})}>4</button>
+        <button className="button1" onClick={()=>this.setState({value:this.state.value+'5'})}>5</button>
+        <button className="button1" onClick={()=>this.setState({value:this.state.value+'6'})}>6</button><br></br>
+        <button className="button1" onClick={()=>this.setState({value:this.state.value+'1'})}>1</button>
+        <button className="button1" onClick={()=>this.setState({value:this.state.value+'2'})}>2</button>
+        <button className="button1" onClick={()=>this.setState({value:this.state.value+'3'})}>3</button><br></br>
+        <button className="button1" onClick={()=>this.setState({value:this.state.value+'0'})}>0</button>
+<h1>ggg</h1>
         </div>
         </div>
-     
+  
        
       
 
